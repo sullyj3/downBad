94e6c61426e62fac0564d5a3b8a7bee970ffad1d
James Sully <sullyj3@gmail.com>
2022-07-27 14:48:32 +1000

    Client can connect to server

--- downbad/Main.hs
+++ downbad/Main.hs
@@ -1,15 +1,20 @@
-{-# language ImportQualifiedPost #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 
 -- client
 
 module Main (main) where
 
+import Data.ByteString qualified as BS
+import Data.ByteString.UTF8 qualified as UTF8
+import Network.Run.TCP (runTCPClient)
+import Network.Socket (Socket)
 import Network.Socket qualified as Socket
-import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
-import Ki.Unlifted
+import Network.Socket.ByteString (recv, sendAll)
+import System.IO (BufferMode (..), Handle, hGetLine, hPutStrLn, hSetBuffering)
 
-
-port = 6776
+port = "6777"
 
 main :: IO ()
-main = pure ()
+main = do
+  runTCPClient "127.0.0.1" port $ \sock -> do
+    sendAll sock (UTF8.fromString "Hello, server!")
